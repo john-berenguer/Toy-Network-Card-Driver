3961cb6c05f1d2e6e8df0610c7daf1c26b8529b1
john-berenguer <johnpberenguer@gmail.com>
2026-02-10 13:48:04 +0100

    Implement basic TCP protocol handling in tcp.c

---
+++ tcp.c
@@ -0,0 +1,181 @@
+#include "tcp.h"
+#include "ip.h"
+#include "interface.h"
+
+#include <string.h>
+#include <arpa/inet.h>
+
+#define TCP_FLAG_FIN 0x01
+#define TCP_FLAG_SYN 0x02
+#define TCP_FLAG_ACK 0x10
+
+#define TCP_STATE_LISTEN       0
+#define TCP_STATE_SYN_RECEIVED 1
+#define TCP_STATE_ESTABLISHED  2
+#define TCP_STATE_FIN          3
+
+struct tcp_header {
+    uint16_t src_port;
+    uint16_t dst_port;
+    uint32_t seq;
+    uint32_t ack;
+    uint8_t  offset_reserved;
+    uint8_t  flags;
+    uint16_t window;
+    uint16_t checksum;
+    uint16_t urgent;
+} __attribute__((packed));
+
+static int tcp_state = TCP_STATE_LISTEN;
+static uint32_t local_seq;
+static uint32_t remote_seq;
+static uint16_t remote_port;
+static uint32_t remote_ip;
+
+/* Checksum TCP (pseudo-header mínimo) */
+static uint16_t tcp_checksum(const struct ip_header *ip,
+                             const struct tcp_header *tcp,
+                             const uint8_t *payload,
+                             size_t payload_len)
+{
+    uint32_t sum = 0;
+
+    sum += (ip->src_ip >> 16) & 0xFFFF;
+    sum += ip->src_ip & 0xFFFF;
+    sum += (ip->dst_ip >> 16) & 0xFFFF;
+    sum += ip->dst_ip & 0xFFFF;
+    sum += htons(IPPROTO_TCP);
+    sum += htons(sizeof(struct tcp_header) + payload_len);
+
+    const uint16_t *buf = (const uint16_t *)tcp;
+    size_t len = sizeof(struct tcp_header);
+
+    while (len > 1) {
+        sum += *buf++;
+        len -= 2;
+    }
+
+    buf = (const uint16_t *)payload;
+    len = payload_len;
+
+    while (len > 1) {
+        sum += *buf++;
+        len -= 2;
+    }
+
+    if (len)
+        sum += *(uint8_t *)buf;
+
+    while (sum >> 16)
+        sum = (sum & 0xFFFF) + (sum >> 16);
+
+    return ~sum;
+}
+
+static void tcp_send(const struct ip_header *rx_ip,
+                     uint8_t flags,
+                     const uint8_t *payload,
+                     size_t payload_len)
+{
+    uint8_t buffer[1500];
+
+    struct ip_header *ip =
+        (struct ip_header *)buffer;
+    struct tcp_header *tcp =
+        (struct tcp_header *)(buffer + sizeof(struct ip_header));
+
+    ip->ver_ihl = 0x45;
+    ip->tos = 0;
+    ip->total_length =
+        htons(sizeof(struct ip_header) +
+              sizeof(struct tcp_header) +
+              payload_len);
+    ip->id = 0;
+    ip->flags_frag = 0;
+    ip->ttl = 64;
+    ip->protocol = IPPROTO_TCP;
+    ip->src_ip = rx_ip->dst_ip;
+    ip->dst_ip = rx_ip->src_ip;
+    ip->checksum = 0;
+    ip->checksum = ip_checksum(ip, sizeof(struct ip_header));
+
+    tcp->src_port = htons(80);
+    tcp->dst_port = htons(remote_port);
+    tcp->seq = htonl(local_seq);
+    tcp->ack = htonl(remote_seq);
+    tcp->offset_reserved = (5 << 4);
+    tcp->flags = flags;
+    tcp->window = htons(1024);
+    tcp->urgent = 0;
+    tcp->checksum = 0;
+
+    if (payload_len)
+        memcpy(buffer + sizeof(struct ip_header) +
+               sizeof(struct tcp_header),
+               payload,
+               payload_len);
+
+    tcp->checksum =
+        tcp_checksum(ip, tcp,
+                     payload, payload_len);
+
+    send_ip_packet(ip,
+                   buffer + sizeof(struct ip_header),
+                   sizeof(struct tcp_header) + payload_len);
+}
+
+void tcp_handle(const struct ip_header *ip,
+                const uint8_t *payload,
+                size_t len)
+{
+    if (len < sizeof(struct tcp_header))
+        return;
+
+    struct tcp_header *tcp =
+        (struct tcp_header *)payload;
+
+    uint16_t dst_port = ntohs(tcp->dst_port);
+
+    if (dst_port != 80)
+        return;
+
+    uint8_t flags = tcp->flags;
+    uint32_t seq = ntohl(tcp->seq);
+
+    switch (tcp_state) {
+
+    case TCP_STATE_LISTEN:
+        if (flags & TCP_FLAG_SYN) {
+            remote_ip = ip->src_ip;
+            remote_port = ntohs(tcp->src_port);
+            remote_seq = seq + 1;
+            local_seq = 1;
+
+            tcp_send(ip,
+                     TCP_FLAG_SYN | TCP_FLAG_ACK,
+                     NULL, 0);
+
+            tcp_state = TCP_STATE_SYN_RECEIVED;
+        }
+        break;
+
+    case TCP_STATE_SYN_RECEIVED:
+        if (flags & TCP_FLAG_ACK) {
+            tcp_state = TCP_STATE_ESTABLISHED;
+        }
+        break;
+
+    case TCP_STATE_ESTABLISHED:
+        if (flags & TCP_FLAG_FIN) {
+            remote_seq = seq + 1;
+            tcp_send(ip,
+                     TCP_FLAG_ACK | TCP_FLAG_FIN,
+                     NULL, 0);
+            tcp_state = TCP_STATE_FIN;
+        }
+        break;
+
+    default:
+        break;
+    }
+}
