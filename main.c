2fa80af5fdc297016551dc29bcccaa97aa5de05f
john-berenguer <johnpberenguer@gmail.com>
2026-02-10 14:00:03 +0100

    Refactor packet handling and improve structure

--- main.c
+++ main.c
@@ -5,73 +5,65 @@
 #include <arpa/inet.h>
 
 #include "interface.h"
+#include "arp.h"
+#include "ip.h"
 
-void received_packet(const void *data, unsigned int length) {
-    printf("Received packet of length %u [", length);
-    const unsigned char *bytes = (const unsigned char *)data;
-    for (unsigned int i = 0; i < length && i < 10; i++) {
-        printf("%02x ", bytes[i]);
+/* Ethernet RX callback */
+void received_packet(const void *data, unsigned int length)
+{
+    if (length < 14)
+        return;
+
+    const uint8_t *frame = (const uint8_t *)data;
+
+    uint16_t ethertype = ntohs(*(uint16_t *)(frame + 12));
+
+    switch (ethertype) {
+
+    case 0x0806: // ARP
+        arp_handle(frame + 14, length - 14);
+        break;
+
+    case 0x0800: // IPv4
+        ip_handle(frame + 14, length - 14);
+        break;
+
+    default:
+        break;
     }
-    printf("]\n");
 }
 
-struct ethernet_frame {
-    unsigned char dest_mac[6];
-    unsigned char src_mac[6];
-    unsigned short ethertype;
-    unsigned char payload[1500]; //Hardcoded mtu for testing!!!
-} __attribute__((packed));
-
-unsigned int test_ethernet(unsigned char * buffer, const unsigned char *src, const unsigned char *dst, unsigned short ethertype, const unsigned char *payload, unsigned int payload_length) {
-    struct ethernet_frame *frame = (struct ethernet_frame *)buffer;
-    for (int i = 0; i < 6; i++) {
-        frame->dest_mac[i] = dst[i];
-    }
-    for (int i = 0; i < 6; i++) {
-        frame->src_mac[i] = src[i];
-    }
-    frame->ethertype = htons(ethertype);
-    memcpy(frame->payload, payload, payload_length);
-    return sizeof(struct ethernet_frame) - 1500 + payload_length; //Hardcoded mtu for testing!!!
-}
-
-int main(int argc, char* argv[]) {
+int main(int argc, char* argv[])
+{
     nic_device_t nic;
-    nic_driver_t * drv = nic_get_driver();
+    nic_driver_t *drv = nic_get_driver();
 
     if (drv->init(&nic) != STATUS_OK) {
         printf("Failed to initialize NIC\n");
         return -1;
     }
-    if (drv->ioctl(&nic, NIC_IOCTL_ADD_RX_CALLBACK, (void *)&received_packet) != STATUS_OK) {
+
+    /* IP fija (CAMBIA si hace falta) */
+    uint32_t my_ip = inet_addr("192.168.56.100");
+
+    arp_init(my_ip, nic.mac_address);
+    ip_init(my_ip);
+
+    if (drv->ioctl(&nic, NIC_IOCTL_ADD_RX_CALLBACK,
+                   (void *)&received_packet) != STATUS_OK) {
         printf("Failed to add RX callback\n");
         drv->shutdown(&nic);
         return -1;
     }
 
-    unsigned char buffer[2048];
-    unsigned int packet_length = test_ethernet(
-        buffer,
-        nic.mac_address, // Source MAC
-        (unsigned char[]){0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF}, // Broadcast MAC
-        0x9000, // Ethertype for testing
-        (unsigned char *)"Hello, this is a test payload", //Payload content
-        29 // Payload length
-    );
-    
-    if (drv->send_packet(&nic, buffer, packet_length) != STATUS_OK) {
-        printf("Failed to send packet\n");
-        drv->shutdown(&nic);
-        return -1;
+    printf("NIC up. Listening for packets...\n");
+
+    /* Mantener el programa vivo */
+    while (1) {
+        sleep(1);
     }
 
-    //Wait for key press to exit
-    printf("Press Enter to exit...\n");
-    getchar();
-
-    if (drv->shutdown(&nic) != STATUS_OK) {
-        printf("Failed to shutdown NIC\n");
-        return -1;
-    }
+    drv->shutdown(&nic);
     return 0;
 }
+
